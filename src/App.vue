521dc1978c4ed3d3dc47d7fe9361f05b0c8ef583
王旭杰 <280597526@qq.com>
2017-06-13 17:16:23 +0800

    导航选中问题

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,9 @@
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li><router-link to="/card">{{ tab1 }}</router-link></li>
-            <li><router-link to="/tab2">{{ tab2 }}</router-link></li>
-            <li><router-link to="/tab3">{{ tab3 }}</router-link></li>
+            <li class="active" @click="change_active(0)"><router-link to="/card">{{ tab1 }}</router-link></li>
+            <li @click="change_active(1)"><router-link to="/tab2">{{ tab2 }}</router-link></li>
+            <li @click="change_active(2)"><router-link to="/tab3">{{ tab3 }}</router-link></li>
           </ul>
         </div>
       </div>
@@ -33,6 +33,12 @@ export default {
       tab2 : '组卡',
       tab3 : '卡组',
     }
+  },
+  methods:{
+    change_active(obj) {
+      $('.nav').find('li').eq(obj).attr('class','active');
+      $('.nav').find('li').eq(obj).siblings().attr('class','');
+    },
   }
 }
 </script>
