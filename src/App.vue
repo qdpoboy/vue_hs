575f22bc4fffe5f54ef6bcf0c2e886b20e4ba928
王旭杰 <280597526@qq.com>
2017-07-23 22:52:05 +0800

    导航选中问题

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <nav class="navbar navbar-default">
       <div class="container-fluid container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -13,9 +14,15 @@
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li @click="change_active(0)"><router-link to="/card">{{ tab1 }}</router-link></li>
-            <li @click="change_active(1)"><router-link to="/build">{{ tab2 }}</router-link></li>
-            <li @click="change_active(2)"><router-link to="/tab3">{{ tab3 }}</router-link></li>
+            <li @click="change_active(0)">
+              <router-link to="/card">{{ tab1 }}</router-link>
+            </li>
+            <li @click="change_active(1)">
+              <router-link to="/build">{{ tab2 }}</router-link>
+            </li>
+            <li @click="change_active(2)">
+              <router-link to="/tab3">{{ tab3 }}</router-link>
+            </li>
           </ul>
         </div>
       </div>
@@ -25,32 +32,33 @@
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data: function() {
-    return {
-      tab1 : '卡牌',
-      tab2 : '组卡',
-      tab3 : '卡组',
-    }
-  },
-  mounted: function(){
-    console.log(this.$route);
-    if(this.$route.path == '/' || this.$route.path == '/card'){
-      this.change_active(0);
-    }
-  },
-  methods:{
-    change_active(obj) {
-      $('.nav').find('li').eq(obj).attr('class','active');
-      $('.nav').find('li').eq(obj).siblings().attr('class','');
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        tab1: '卡牌',
+        tab2: '组卡',
+        tab3: '卡组',
+      }
     },
+    mounted: function () {
+      if (this.$route.path == '/' || this.$route.path == '/card') {
+        this.change_active(0);
+      } else if (this.$route.path == '/build') {
+        this.change_active(1);
+      }
+    },
+    methods: {
+      change_active(obj) {
+        $('.nav').find('li').eq(obj).attr('class', 'active');
+        $('.nav').find('li').eq(obj).siblings().attr('class', '');
+      },
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  color: #2c3e50;
-}
+  #app {
+    color: #2c3e50;
+  }
 </style>
