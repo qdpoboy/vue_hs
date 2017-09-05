51c3f4181cf01e955bf0cfb13eec203934806525
王旭杰 <280597526@qq.com>
2017-09-05 23:21:13 +0800

    重新获取卡组数据，新增冰封王座的骑士 增加吸血字段

--- python/hs_17173.py
+++ python/hs_17173.py
@@ -22,15 +22,19 @@ def getPage(page):
 
     if json_data['status'] == 'success':
         card_data = json_data['data']
-        sql = "insert into hs_card (name,career,card_set,card_type,cost,race,atk,health,rarity,windfury,taunt,freeze,battlecry,stealth,combo,charge,grant_charge,spellpower,silence,enrage,divine_shield,deathrattle,secret,mission,img_url,card_url) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        sql = "insert into hs_card (name,career,card_set,card_type,cost,race,atk,health,rarity,windfury,taunt,freeze,battlecry,stealth,combo,charge,grant_charge,spellpower,silence,enrage,divine_shield,deathrattle,secret,mission,lifesteal,img_url,card_url) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         for one in card_data:
             img_url  = 'http://i1.17173cdn.com/8hpoty/YWxqaGBf/images/resource/new_middler/' + one['card_id'] + '.png?version=201612021432'
             card_url = 'http://cha.17173.com/hs/info/card_zhcn/' + str(one['id'])
-            sql_data = (one['name'],one['class'],one['card_set'],one['card_type'],one['cost'],one['race'],one['atk'],one['health'],one['rarity'],one['windfury'],one['taunt'],one['freeze'],one['battlecry'],one['stealth'],one['combo'],one['charge'],one['grant_charge'],one['spellpower'],one['silence'],one['enrage'],one['divine_shield'],one['deathrattle'],one['secret'],one['mission'],img_url,card_url)
+            if one.has_key('lifesteal'):
+                lifesteal = one['lifesteal']
+            else:
+                lifesteal = 0
+            sql_data = (one['name'],one['class'],one['card_set'],one['card_type'],one['cost'],one['race'],one['atk'],one['health'],one['rarity'],one['windfury'],one['taunt'],one['freeze'],one['battlecry'],one['stealth'],one['combo'],one['charge'],one['grant_charge'],one['spellpower'],one['silence'],one['enrage'],one['divine_shield'],one['deathrattle'],one['secret'],one['mission'],lifesteal,img_url,card_url)
             my.insertOne(sql, sql_data)
         getPage(page+1)
     else:
-       print "第" + str(page) + "条获取失败" + json_data['status']
+       print "第" + str(page) + "页获取失败" + json_data['status']
 
 if __name__ == "__main__":
     getPage(1)
